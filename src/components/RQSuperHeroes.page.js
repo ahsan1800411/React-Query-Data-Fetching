369865f1ae3a4b7f06655d1ee6f70c769d548f6d
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-10-07 15:15:30 +0500

    Handling Error Query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,13 +2,19 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
+  return axios.get("http://localhost:4000/superheroes1");
 };
 
 const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery("super-hero", fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery(
+    "super-hero",
+    fetchSuperHeroes
+  );
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h3>{error.message}</h3>;
   }
 
   return (
